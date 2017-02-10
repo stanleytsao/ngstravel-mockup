fbf8d706d170a87c2b1187fd4d510f18965f12ab
Stanley Tsao <stsao002@gmail.com>
2017-02-10 10:58:43 -0800

    move header and footer into php includes

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+	<title>故鄉旅遊 - NGS Travel</title>
+</head>
+<body>
+	
+	<?php include("includes/header.html");?>
+
+	
+
+	<!-- Slider -->
+	<section class="slider container-fluid text-center">
+		Slider
+		(4-5 Promotional Items)
+	</section>
+
+	<!-- Main Content -->
+	<div class="container content">
+		<div class="row">
+			<div class="col-md-10">
+				<div class="row">
+					<div class="col-md-7 box1">
+						<h2>New Packages</h2>
+						<ul>
+							<li class="item">item1</li>
+							<li class="item">item2</li>
+							<li class="item">item3</li>
+						</ul>
+					</div>
+					<div class="col-md-5 box2">
+						<h2>Most Popular</h2>
+						<ul>
+							<li class="item">item1</li>
+							<li class="item">item2</li>
+							<li class="item">item3</li>
+						</ul>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 box3">Box3</div>
+				</div>
+			</div>
+			<div class="col-md-2 box4">Ads</div>
+		</div>
+	</div>
+	
+	<?php include("includes/footer.html");?>
+
+</body>
+</html>
